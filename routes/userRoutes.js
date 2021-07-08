5652c7814942c7c0524b5b5131cb71a7529b0354
USER <udaraj08@gmail.com>
2021-07-08 08:45:24 +0530

    user signup complete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,70 @@
 const express = require('express')
 const route = express.Router()
 
+///use modals
 const userModal = require('../DB/User')
 
-////////////////////////////////////
+//use bcrypt
+const bcrypt = require('bcrypt')
+
+///////////Handle Error/////////////
+const handleErrorMsg = (usernameMsg, passwordMsg, status) => {
+
+    const errorMsg = {
+        username: usernameMsg,
+        password: passwordMsg,
+        satus: status
+    }
+
+    return errorMsg
+}
+
+
+//////////////////////////////////////
 ////////POST Requests////////////////
 ////////////////////////////////////
-route.post('/user/validate', (req, res) => {
+route.post('/user/login', async (req, res) => {
 
-    res.send("HEllo this is the user !!!")
+    const { username, password } = req.body
+
+    const data = await userModal.findOne({ "username": username })
+
+    if (data) {
+        const auth = await bcrypt.compare(password, data.password)
+
+        if (auth)
+            res.send(handleErrorMsg("", "", true))
+        else
+            res.send(handleErrorMsg("", "invalid password", false))
+
+    } else {
+        res.send(handleErrorMsg("invalid user", "", false))
+    }
+})
+
+route.post('/user/signup', async (req, res) => {
+
+    const { username, password } = req.body
+
+    const salt = await bcrypt.genSalt()
+    const hashedPW = await bcrypt.hash(password, salt)
+
+    const data = {
+        username: username,
+        password: hashedPW
+    }
+    await userModal.create(data).
+        then(data => {
+            res.send(data)
+        }).catch(err => {
+            if (err)
+                res.send(err.message)
+        })
+})
+
+route.post('/user/forgotPW', (req, res) => {
+
+    res.send()
 })
 
 module.exports = route
